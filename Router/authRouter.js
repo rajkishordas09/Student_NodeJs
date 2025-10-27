cd98972d295c63820d8d4bfa28e916ac4e641dec
Raj <Raj@gmail.com>
2025-10-27 15:28:49 +0530

    add jwt token in login api

--- Router/authRouter.js
+++ Router/authRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRouter = express.Router();
 const User = require("../Model/user")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 authRouter.post("/signup", async (req, res) => {
   try {
     const { username, email, password, role } = req.body
@@ -16,17 +17,27 @@ authRouter.post("/signup", async (req, res) => {
   }
 })
 authRouter.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const isUser = await User.findOne({ username })
-  if (!isUser) {
-    return res.status(404).send("username not found")
+  try {
+    const { username, password } = req.body;
+    const isUser = await User.findOne({ username })
+    if (!isUser) {
+      return res.status(404).send("username not found")
+    }
+    const ispasswordMatch = await bcrypt.compare(password, isUser.password)
+    if (!ispasswordMatch) {
+      return res.status(404).send("password not matched")
+    }
+
+    const jwtToken = jwt.sign(
+      { username: isUser.username, role: isUser },
+      'your_jwt_secret',
+      { expiresIn: '1d' }
+    );
+    res.status(201).json({ message: "login successfully", jwtToken })
+
   }
-  const ispasswordMatch = await bcrypt.compare(password, isUser.password)
-  if (!ispasswordMatch) {
-    return res.status(404).send("password not matched")
-  }
-  else {
-    res.status(201).json({ message: "login successfully", isUser })
+  catch (error) {
+    res.status(404).send({ error: error.message })
   }
 })
 
